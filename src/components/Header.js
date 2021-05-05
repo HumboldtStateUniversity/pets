2d437029b3a9045c1ff98d234d5bd2f3c4b0c0b3
Matt Hodgson <mmh61@humboldt.edu>
2021-05-05 08:38:06 -0700

    First basic build of site

---
+++ src/components/Header.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function Header(props) {
+  return <h1 className="page-title">{props.headerText}</h1>;
+}
---
+++ src/pages/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Header from "../components/Header";
+import Gallery from "../components/Gallery";
+
+const myPets = () => {
+  return (
+    <>
+      <Header headerText="Pets of HSU Advancement" />
+      <Gallery />
+    </>
+  );
+};
+export default myPets;
